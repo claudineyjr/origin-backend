2aba48fb6011fb922d2db9a4ab6f6f3d4efeef2a
Claudiney Junior <claudiney.info@gmail.com>
2021-11-28 22:06:12 -0300

    Removing compiling files

--- src/insurance/dtos/insurance-risk-response.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.InsuranceRiskResponseDto = void 0;
-var InsuranceRiskResponseDto = /** @class */ (function () {
-    function InsuranceRiskResponseDto() {
-    }
-    return InsuranceRiskResponseDto;
-}());
-exports.InsuranceRiskResponseDto = InsuranceRiskResponseDto;
